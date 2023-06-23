2f5691c03abc18815678d45c5acae04ff0e5b3eb
Vladimir <vladimirkucev6@gmail.com>
2023-06-23 12:28:31 +0300

    event system addet

--- include/events/Event.hpp
+++ include/events/Event.hpp
@@ -9,17 +9,18 @@ enum class EventType
 	WindowClose,
 	WindowResize,
 	WindowFocus,
+	WindowLostFocus,
 	WindowMoved,
 	AppTick,
 	AppUpdate,
 	AppRender,
-	KeyPress,
-	KeyRelease,
+	KeyPressed,
+	KeyReleased,
 	MouseButtonPressed,
 	MouseButtonReleased,
 	MouseMoved,
-	MouseScrolled,
-}
+	MouseScrolled
+};
 
 enum EventCategory
 {
@@ -32,7 +33,7 @@ enum EventCategory
 };
 
 #define EVENT_CLASS_TYPE(type)                                                  \
-	static EventType GetStaticType() { return EventType::type; }                \
+	static EventType GetStaticType() { return EventType::type; }              \
 	virtual EventType GetEventType() const override { return GetStaticType(); } \
 	virtual const char *GetName() const override { return #type; }
 
@@ -41,37 +42,41 @@ enum EventCategory
 
 class Event
 {
+	friend class EventDispatcher;
+
 public:
-	virtual ~Event() = default;
-
-	bool Handled = false;
-
 	virtual EventType GetEventType() const = 0;
 	virtual const char *GetName() const = 0;
 	virtual int GetCategoryFlags() const = 0;
 	virtual std::string ToString() const { return GetName(); }
 
-	bool IsInCategory(EventCategory category)
+	inline bool IsInCategory(EventCategory category)
 	{
 		return GetCategoryFlags() & category;
 	}
+
+protected:
+	bool m_Handled = false;
 };
 
 class EventDispatcher
 {
+	template <typename T>
+	using EventFn = std::function<bool(T &)>;
+
 public:
 	EventDispatcher(Event &event)
 		: m_Event(event)
 	{
+
 	}
 
-	// F will be deduced by the compiler
-	template <typename T, typename F>
-	bool Dispatch(const F &func)
+	template <typename T>
+	bool Dispatch(EventFn<T> func)
 	{
 		if (m_Event.GetEventType() == T::GetStaticType())
 		{
-			m_Event.Handled |= func(static_cast<T &>(m_Event));
+			m_Event.m_Handled = func(*(T *)&m_Event);
 			return true;
 		}
 		return false;
--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,14 @@
 #include "Engine/Engine.hpp"
 #include "debug/Log.hpp"
+#include "Events/ApplicationEvent.hpp"
 int main() {
     Engine engine;
     Log::Init();
     TUG_CORE_ERROR("Tugine Engine");
     TUG_INFO("Engine client");
+
+    WindowResizeEvent e(1280, 720);
+    TUG_TRACE(e);
     engine.Initialize();
     engine.MainLoop();
 
