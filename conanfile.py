a820713382a3913349671bdbb8b7d5d1f7c536c7
Vladimir Kutsev <vladimirkucev6@gmail.com>
2023-10-02 19:07:05 +0300

    cleen all

--- conanfile.py
+++
@@ -1,8 +0,0 @@
-from conan import ConanFile
-
-class ConanTUgineRecipe(ConanFile):
-    settings = "os", "compiler", "build_type", "arch"
-    generators = "PremakeDeps"
-
-    def requirements(self):
-        self.requires("spdlog/1.11.0")
--- src/main.cpp
+++
@@ -1,8 +0,0 @@
-#include <iostream>
-
-#include <spdlog/spdlog.h>
-
-int main(){
-    spdlog::warn("sup bitch");
-    return 0;
-}
